7442a38a985e47f10d3f90887571b4fa320f7149
Ahmad09 <ahmad.khan8747763@gmail.com>
2026-08-27 12:23:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,4 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"welcome": "hello ahmad khan i am a student of computer science"}
-    @app.get("/about")
-def about():
-    return {"message": "i am student of computer science"}
+    
